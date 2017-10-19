6b08f280e8a38332046285d279c92824cda2a17d
Yosshi999 <yosshi999.for.entry@gmail.com>
2017-10-19 17:21:54 +0900

    highlight last position

--- game.py
+++ game.py
@@ -6,15 +6,19 @@ from time import sleep
 AI = [simpleAIs.MonteTreeAI(0, 50, 3), simpleAIs.MonteAI(1, 10)]
 
 print("0:\033[46m  \033[0m 1:\033[43m  \033[49m")
-def printObs(obs):
+def printObs(obs, lastRow, lastLine):
     for i in range(8):
         for j in range(8):
-            if obs[i, j] == 0:
-                print("\033[46m  \033[49m", end="")
-            elif obs[i, j] == 1:
-                print("\033[43m  \033[49m", end="")
+            if i == lastRow and j == lastLine:
+                st = "::"
             else:
-                print("\033[47m  \033[49m", end="")
+                st = "  "
+            if obs[i, j] == 0:
+                print("\033[46m%s\033[49m"%st, end="")
+            elif obs[i, j] == 1:
+                print("\033[43m%s\033[49m"%st, end="")
+            else:
+                print("\033[47m%s\033[49m"%st, end="")
         print()
     #print()
 
@@ -32,12 +36,13 @@ for i in range(1, n_episodes+1):
     # print("\033[8B", end="")
     print("\n"*7)
     print("\033[8A\r", end="")
-    printObs(obs)
+    printObs(obs, -1, -1)
 
     while not done:
-        obs, r, done, info = env.step(AI[turn].act(obs))
+        action = AI[turn].act(obs)
+        obs, r, done, info = env.step(action)
         print("\033[8A\r", end="")
-        printObs(obs)
+        printObs(obs, action//8, action%8)
         
         if r == 1:
             print("win:", turn)
